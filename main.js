23e8b641cc508395edf4cf7dce3bc7fd774ce795
Piotr Koch <ta2.5.pk@wp.pl>
2020-05-13 19:17:25 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const firstname='Piotr';
+const age=34;
+console.log(`Cześć, nazywam się ${firstname} i mam ${age} lat. Chciałbym cię przywitać na mojej nowej stronie`);
+alert(`Cześć, nazywam się ${firstname} i mam ${age} lat. Chciałbym cię przywitać na mojej nowej stronie`);
